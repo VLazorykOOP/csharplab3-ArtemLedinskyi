9aa3c4b91f41b9f5b53f1493bdf285d432c6a3c0
Артем Лединський <124506074+ArtemLedinskyi@users.noreply.github.com>
2024-02-29 21:40:51 +0200

    Update TransportVehicle.cs

--- Lab3CSharp/TransportVehicle.cs
+++ Lab3CSharp/TransportVehicle.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lab3CSharp
 {
     internal class TransportVehicle
     {
+        public string Name { get; set; }
+        public int MaxSpeed { get; set; }   
+
+        public virtual void Show()
+        {
+            Console.WriteLine($"Транспортний засіб: {Name} , максимальна швидкість: {MaxSpeed}");
+        }
+
     }
 }
